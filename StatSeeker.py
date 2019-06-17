b6ff3d485427f4ab35283358c511664226449095
jjp31 <jjp31@kent.ac.uk>
2019-06-17 13:23:46 +0100

    Small cleanup

--- StatSeeker.py
+++ StatSeeker.py
@@ -1,12 +1,10 @@
 import argparse
-import sys
-import glob
 import os
 from batteries import ent
 from utilities import dir_path, ent_csv
 
 
-def main(argv):
+def main():
     opts = argparse.ArgumentParser(prog='StatSeeker', description=__doc__)
 
     opts.add_argument('-a', action='store_true', help="run all test batteries")
@@ -14,20 +12,21 @@ def main(argv):
     opts.add_argument("--path", type=dir_path, help="one or more files to process")
 
     args = opts.parse_args()
-    f = []
-    e = []
 
+    file_names = []
+    ent_results = []
     for root, dir, files in os.walk(args.path):
         for file in files:
-            fs = os.stat(root+file)
+            path = os.path.join(root, file)
+            fs = os.stat(path)
             if fs.st_size >= 1024:
-                f.append(root+file)
-                e.append(ent(root+file))
+                file_names.append(path)
+                ent_results.append(ent(path))
 
-    # print(f)
-    # print(e)
-    ent_csv(f, e)
+    # print(file_names)
+    # print(ent_results)
+    ent_csv(file_names, ent_results)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
