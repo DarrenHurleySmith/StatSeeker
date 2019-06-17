6bbe5c1249e30b605cc2809fa3cc2ed70b2b3440
DarrenHurleySmith <d.hurleysmith@gmail.com>
2019-06-17 17:22:44 +0100

    fixed long run test

--- batteries.py
+++ batteries.py
@@ -31,9 +31,6 @@ def fips140(fn, ver, p):
         if fs.st_size/(2500*params) < 1:
             params = int(fs.st_size/2500)
 
-        #if params == 0:
-        #    return ['N', 'N', 'N', 'N', 'N'], [0, 0, 0, 0]
-
         for i in range(params):
             stat = []
             r = []
